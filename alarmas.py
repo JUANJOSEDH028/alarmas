efae5f5712291c9b25fcd38d5948f4297b8c2573
JUANJOSEDH028 <juanjosalazar0228@gmail.com>
2025-02-24 12:18:42 -0500

    Update alarmas.py

--- alarmas.py
+++ alarmas.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import os
 
 # Ruta del archivo CSV relativa
-file_path = "https://github.com/JUANJOSEDH028/alarmas/raw/main/AlarmHistory.csv"
+file_path = "https://github.com/JUANJOSEDH028/alarmas/raw/main/AlarmHistory1.csv"
 
 # Leer y limpiar los datos
 data = pd.read_csv(
